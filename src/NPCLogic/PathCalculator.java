01fd351ddfc973f11263de8173cb2f8e5d3b9afc
Arne de Beer <arnedebeer2001@gmail.com>
2020-03-16 16:39:34 +0100

    [Refactor] Instead of MapName directly pass DistanceMap to PathCalculator

--- src/NPCLogic/PathCalculator.java
+++ src/NPCLogic/PathCalculator.java
@@ -8,15 +8,14 @@ public class PathCalculator {
      * Calculates the next target for a NPCLogic.Person, allows movement in 8 directions
      *
      * @param currPos the current position of the character
-     * @param mapName the name of the map of the destination of the character
+     * @param distanceMap map of all distances relative to target position
      * @return the next position the character moves to
      */
-    public static Point2D nextTarget(Point2D currPos, String mapName) {
+    public static Point2D nextPositionToTarget(Point2D currPos, DistanceMap distanceMap) {
         // TODO: Not hardcode this 32 tilesize and retrieve from tile logic
         int Xindex = (int) Math.floor(currPos.getX() / 32.0);
         int Yindex = (int) Math.floor(currPos.getY() / 32.0);
 
-        NPCLogic.DistanceMap distanceMap = null; //Main.getDistanceMap(mapName);
         Point2D middlePointCoords = new Point2D.Double(distanceMap.getTarget().getMiddlePoint().getX() * 32, distanceMap.getTarget().getMiddlePoint().getY() * 32);
 
         if (middlePointCoords.distance(currPos) <= 32) {
@@ -49,11 +48,10 @@ public class PathCalculator {
      * Finds a random tile that is walkable and closest to a point
      *
      * @param currentPosition the position the character is currently on
-     * @param mapName         the name of the DistanceMap it is walking on
+     * @param distanceMap the DistanceMap it is walking on
      * @return the available tile, if not found return the currentPosition
      */
-    public static Point2D findRandomClosestWalkable(Point2D currentPosition, String mapName) {
-        NPCLogic.DistanceMap distanceMap = null;//Main.getDistanceMap(mapName);
+    public static Point2D findRandomClosestWalkable(Point2D currentPosition, DistanceMap distanceMap) {
         int failedAttempts = 0;
         while (failedAttempts < 8) {
             int xPos = (int) Math.floor(Math.random() * 2.9) - 1 + ((int) currentPosition.getX()) / 32;
@@ -63,7 +61,7 @@ public class PathCalculator {
             yPos = Math.min(yPos, 99);
             yPos = Math.max(0, yPos);
             boolean[][] walkableMap = distanceMap.getWalkableMap();
-            if (walkableMap[xPos][yPos] == true) {
+            if (walkableMap[xPos][yPos]) {
                 return new Point2D.Double(xPos * 32, yPos * 32);
             }
             failedAttempts++;
