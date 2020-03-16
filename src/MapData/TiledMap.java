c9967e9c894ba2af90233b8dc9c4f58b10db0f7b
Rik <rik.vos01@gmail.com>
2020-03-16 11:59:31 +0100

    [Refactor] refactored and added comments to every tiled class

--- src/MapData/TiledMap.java
+++ src/MapData/TiledMap.java
@@ -11,38 +11,56 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * this object is the mother object of the map
+ * all the tiled layers are stored here
+ */
 public class TiledMap implements Drawable {
-
+    //static finals for where you can find the sprites and the layout json file
     private final static String SPRITESHEETS_DIR = "Resources/spritesheets/";
     private final static String MAP_LAYOUT_DIR = "Resources/festmap.json";
 
-    private static int MAP_WIDTH = 100;//todo kunnen we deze niet beter op 0 initialiseren zodat je nooit out of bounds exceptions krijgt
-    private static int MAP_HEIGHT = 100;
-    private static int TILE_SIZE = 32;
+    //values for the dimensions of the map
+    private static int MAP_WIDTH;
+    private static int MAP_HEIGHT;
+    private static int TILE_SIZE;
 
+    //arraylist where the layers are stored
     private ArrayList<TiledLayer> tiledLayers;
 
+    /**
+     * constructor
+     * first the TiledMapImage is created
+     * then the json file is read out and for each layer a new object is created
+     */
     public TiledMap() {
         this.tiledLayers = new ArrayList<>();
+
+        //create the tiledMapImage object so we can add to it the sprites later
         TiledMapImage tiledMapImage = new TiledMapImage();
 
         try {
+            //read out the map layout file to an json object
             JsonReader jsonReader = Json.createReader(new FileInputStream(new File(MAP_LAYOUT_DIR)));
             JsonObject baseJsonObject = jsonReader.readObject();
             jsonReader.close();
 
+            //get from the json object the dimensions and set the attributes
             MAP_WIDTH = baseJsonObject.getInt("width");
             MAP_HEIGHT = baseJsonObject.getInt("height");
             TILE_SIZE = baseJsonObject.getInt("tilewidth");
 
+            //read the jsonObject out so we get two array lists containing the data for the layers and tilesets
             JsonArray layersJsonArray = baseJsonObject.getJsonArray("layers");
             JsonArray tilesetsJsonArray = baseJsonObject.getJsonArray("tilesets");
 
+            //for each tileset in the tileset array we add the sprites to the tiledMapImage object so we get access to every sprite by their corresponding gid
             for (JsonObject tileset : tilesetsJsonArray.getValuesAs(JsonObject.class)) {
-                tiledMapImage.initialise(tileset.getString("image"), tileset.getInt("firstgid"),
+                tiledMapImage.addSpriteSheet(tileset.getString("image"), tileset.getInt("firstgid"),
                         tileset.getInt("imagewidth") / TILE_SIZE, tileset.getInt("imageheight") / TILE_SIZE);
             }
 
+            //for each layer we create a tiledLayer object
             for (JsonObject layerJsonObject : layersJsonArray.getValuesAs(JsonObject.class)) {
                 if (layerJsonObject.getBoolean("visible") && !layerJsonObject.getJsonString("type").toString().equals("objectgroup"))
                     tiledLayers.add(new TiledLayer(tiledMapImage, layerJsonObject));
